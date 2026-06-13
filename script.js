a14775cfe679ce813fde1542be467601990c41c9
AmarAbdelmajid <amarbilalabdelmajid@gmail.com>
2026-06-13 20:24:00 +0300

    rps with working UI

--- script.js
+++ script.js
@@ -2,51 +2,71 @@
 
 let humanScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
+
+const selectedBtn = document.querySelectorAll(".btn");
+
+const playerImg = document.querySelector(".player-selection-img");
+
+const computerImg = document.querySelector(".computer-selection-img");
+
+const humanPoints = document.querySelector(".player-points");
+
+const computerPoints = document.querySelector(".computer-points");
+
+const displayRounds = document.querySelector(".played-rounds");
 
 function getComputerChoice() {
   let choice = Math.trunc(Math.random() * 3) + 1;
 
+  let result;
+
   if (choice === 1) {
-    return "rock";
+    result = "rock";
   } else if (choice === 2) {
-    return "paper";
+    result = "paper";
   } else {
-    return "scissors";
+    result = "scissors";
   }
+
+  computerImg.src = `img/${result}.png`;
+  return result;
 }
 
-function getHumanChoice() {
-  let choice = prompt("Enter your choice. rock, paper, scissors");
-  return choice.toLowerCase();
+function getHumanChoice(event) {
+  let playerChoice = event.target.textContent.toLowerCase();
+
+  playerImg.src = `img/${playerChoice}.png`;
+
+  return playerChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
-  console.log(computerChoice);
-  if (humanChoice === computerChoice) return "no one wins";
-  else if (humanChoice === "rock" && computerChoice === "scissors") {
-    humanScore++;
-    return "You won rock beats scissors";
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanScore++;
-    return "You won paper beats rock";
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore++;
-    return "You won scissors beats paper";
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
-    return "Computer won paper beats rock";
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    computerScore++;
-    return "Computer won scissors beats paper";
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore++;
-    return "Computer won rock beats scissors";
+  if (humanScore < 5 && computerScore < 5) {
+    if (humanChoice === computerChoice) {
+      roundsPlayed++;
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      roundsPlayed++;
+    } else {
+      computerScore++;
+      roundsPlayed++;
+    }
+
+    displayRounds.textContent = roundsPlayed;
+    humanPoints.textContent = humanScore;
+    computerPoints.textContent = computerScore;
   }
 }
 
-for (let i = 0; i < 5; i++) {
-  playRound(getHumanChoice(), getComputerChoice());
-}
-
-console.log(humanScore);
-console.log(computerScore);
+selectedBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const humanChoice = getHumanChoice(event);
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
+});
